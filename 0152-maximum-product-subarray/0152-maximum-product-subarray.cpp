c394ed385bc66680a7ae5ec317be08361c5480d3
ankitkum0192 <ankitkum0192@gmail.com>
2026-07-07 22:38:57 +0530

    Time: 0 ms (100%), Space: 17.7 MB (59.85%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int maxProd = nums[0];
+        int currMax = nums[0];
+        int currMin = nums[0];
+        
+        for (int i = 1; i < nums.size(); i++) {
+            int temp = currMax; // currMax update hone se pehle save karna zaroori
+            
+            currMax = max({nums[i], currMax * nums[i], currMin * nums[i]});
+            currMin = min({nums[i], temp * nums[i], currMin * nums[i]});
+            
+            maxProd = max(maxProd, currMax);
+        }
+        
+        return maxProd;
+    }
+};
